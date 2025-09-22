00c0cb6c21eb1ab574510b3e54dc67a1347e140c
Jaime-Poveda2007 <jaime.povedam@uniagustiniana.edu.co>
2025-09-22 19:26:59 +0000

    AGREGAMOS CONTENIDO AL HOME Y A LA CONEXION

--- js/capturados.js
+++ js/capturados.js
@@ -1,4 +1,4 @@
 //ESTA FUNCION ES ORIGIAL, DEBES CAMBIARLE EL NOMBRE RESPECTO A TU API
 function capturados(){
-    alert("capturador")
+    document.getElementById ("root").innerHTML = "capturados"
 }
