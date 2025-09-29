c182b4c4c25c3039228a365f14cef0a73badfe07
Jaime-Poveda2007 <jaime.povedam@uniagustiniana.edu.co>
2025-09-29 20:51:55 +0000

    Seleccionamos numero de pokemones a mostrar

--- js/conexion.js
+++ js/conexion.js
@@ -2,20 +2,18 @@ let pokemones = [];
 let totalPokes = 10;
 
 // Conexión para obtener la lista de Pokémon
-async function conexion() {
+async function conexionLista() {
   const res = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${totalPokes}`);
   const data = await res.json();
-  return data.results;
+  return data;
 }
 
 // Cargar todos los Pokémon al iniciar
 async function General() {
   if (pokemones.length === 0) {
-    pokemones = await conexion();
+    pokemones = await conexionLista();
   }
-  home(pokemones);
-  console.log(pokemones[0].name)
-  
+  Home(pokemones);
 }
 
 General()
